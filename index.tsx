8da6cd401ad3e83e83cb193626212ecb8d6e5659
shantel2012 <kutamatinomudaishe@gmail.com>
2026-03-16 15:36:40 +0200

    first commit

--- index.tsx
+++ index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
